b099b03282e58c54a64e58055bd5cbc8d5360380
Andre Lins <andrelins2403@hotmail.com>
2025-04-14 16:46:51 -0300

    primeiro commit

---
+++ src/main/java/br/lins/sagewallet/SagewalletApplication.java
@@ -0,0 +1,13 @@
+package br.lins.sagewallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagewalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagewalletApplication.class, args);
+	}
+
+}
