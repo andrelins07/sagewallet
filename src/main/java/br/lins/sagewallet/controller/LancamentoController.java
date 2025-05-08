095922030755c4950926974a74d674bead416744
Andre Lins <andrelins2403@hotmail.com>
2025-05-07 22:38:15 -0300

    feat(Controller): cria metodos CRUD da entidade Lancamentos

--- src/main/java/br/lins/sagewallet/controller/LancamentoController.java
+++ src/main/java/br/lins/sagewallet/controller/LancamentoController.java
@@ -1,15 +1,17 @@
 package br.lins.sagewallet.controller;
 
 import br.lins.sagewallet.model.lancamento.Lancamento;
+import br.lins.sagewallet.repository.CategoriaRepository;
 import br.lins.sagewallet.repository.LancamentoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -19,8 +21,62 @@ public class LancamentoController {
     @Autowired
     private LancamentoRepository lancamentoRepository;
 
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
     @GetMapping
     public ResponseEntity<List<Lancamento>> getAll(){
         return ResponseEntity.ok(lancamentoRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Lancamento> findLancamentoById(@PathVariable Long id) {
+
+        return lancamentoRepository.findById(id).map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @GetMapping("/descricao/{descricao}")
+    public ResponseEntity<List<Lancamento>> getCategoriaByNome(@PathVariable String descricao){
+        return ResponseEntity.ok(lancamentoRepository
+                .findAllByDescricaoContainingIgnoreCase(descricao));
+    }
+
+    @PostMapping
+    public ResponseEntity<Lancamento> postPostagem(@Valid @RequestBody Lancamento lancamento) {
+
+        if (categoriaRepository.existsById(lancamento.getCategoria().getId()))
+            return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoRepository.save(lancamento));
+
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria não existe!", null);
+    }
+
+    @PutMapping
+    public ResponseEntity<Lancamento> putPostagem(@Valid @RequestBody Lancamento lancamento) {
+
+        if (lancamentoRepository.existsById(lancamento.getId())) {
+
+            if (categoriaRepository.existsById(lancamento.getCategoria().getId())) {
+                return ResponseEntity.status(HttpStatus.OK).body(lancamentoRepository.save(lancamento));
+            }
+
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria não existe!", null);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deletePostagem(@PathVariable Long id) {
+
+        Optional<Lancamento> postagem = lancamentoRepository.findById(id);
+
+        if (postagem.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        lancamentoRepository.deleteById(id);
     }
 }
--- src/main/java/br/lins/sagewallet/repository/LancamentoRepository.java
+++ src/main/java/br/lins/sagewallet/repository/LancamentoRepository.java
@@ -3,5 +3,10 @@ package br.lins.sagewallet.repository;
 import br.lins.sagewallet.model.lancamento.Lancamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+
+    List<Lancamento> findAllByDescricaoContainingIgnoreCase(String descricao);
+
 }
