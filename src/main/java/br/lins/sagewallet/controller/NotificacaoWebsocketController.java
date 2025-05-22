3b39a8c3710651f9fcb753c93aa115ce1b47c50f
Andre Lins <andrelins2403@hotmail.com>
2025-05-22 16:41:36 -0300

    feat(mocontroller):criacao da classe controladora da entidade Notificacao e do repositorio jpa

---
+++ src/main/java/br/lins/sagewallet/controller/NotificacaoWebsocketController.java
@@ -0,0 +1,28 @@
+package br.lins.sagewallet.controller;
+
+import br.lins.sagewallet.model.Notificacao;
+import br.lins.sagewallet.model.Usuario;
+import br.lins.sagewallet.service.NotificacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NotificacaoWebsocketController {
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    private NotificacaoService notificacaoService;
+
+    @MessageMapping("/exibir-notificacoes")
+    @SendTo("/topics/notificacoes")
+    public void criarNotificacao(Notificacao notificacao) {
+
+        notificacaoService.criarNotificacao(notificacao);
+        messagingTemplate.convertAndSend("/topics/notificacoes", notificacao);
+    }
+}
