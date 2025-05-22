da2358834c2de75c12d052722a5b16870034493a
Andre Lins <andrelins2403@hotmail.com>
2025-05-22 16:39:39 -0300

    feat(model): criação da classe Notificacao

---
+++ src/main/java/br/lins/sagewallet/model/Notificacao.java
@@ -0,0 +1,60 @@
+package br.lins.sagewallet.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_notificacoes")
+public class Notificacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String mensagem;
+
+    private LocalDate dataCriacao;
+
+    @JoinColumn(name = "usuario_id")
+    @ManyToOne
+    private Usuario usuario;
+
+    public Notificacao(Usuario usuario, String mensagem) {
+        this.usuario = usuario;
+        this.mensagem = mensagem;
+        this.dataCriacao = LocalDate.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
