30647205f029e83b95b38c2ca9d9ae60a247eacc
Andre Lins <andrelins2403@hotmail.com>
2025-05-30 22:30:40 -0300

    refactor(notificacao): exclusao do metodo findByUsuario no repositorio notificacao, devido inutilidade

--- src/main/java/br/lins/sagewallet/repository/NotificacaoRepository.java
+++ src/main/java/br/lins/sagewallet/repository/NotificacaoRepository.java
@@ -3,5 +3,6 @@ package br.lins.sagewallet.repository;
 import br.lins.sagewallet.model.Notificacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface NotificacaoRepository extends JpaRepository<Notificacao, Long> {
 }
