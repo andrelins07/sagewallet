d4d2c736a666722ccaa07b6ec40f438f3381e131
Andre Lins <andrelins2403@hotmail.com>
2025-06-19 13:45:02 -0300

    refactor lancamento repository: criacao de querys personalizadas para corrigir problema N + 1

--- src/main/java/br/lins/sagewallet/repository/LancamentoRepository.java
+++ src/main/java/br/lins/sagewallet/repository/LancamentoRepository.java
@@ -4,6 +4,7 @@ import br.lins.sagewallet.model.usuario.Usuario;
 import br.lins.sagewallet.model.lancamento.Lancamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -12,17 +13,32 @@ import java.util.Optional;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 
-    List<Lancamento> findAllByDescricaoContainingIgnoreCase(String descricao);
+    @Query("""
+        SELECT l FROM Lancamento l 
+        JOIN FETCH l.categoria 
+        JOIN FETCH l.usuario 
+        WHERE l.descricao LIKE CONCAT('%', :descricaoParte, '%')
+    """)
+    List<Lancamento> findAllByDescricaoContainingIgnoreCase(@Param("descricaoParte") String descricao);
+
+    @Query("""
+        SELECT l FROM Lancamento l 
+        JOIN FETCH l.categoria 
+        JOIN FETCH l.usuario 
+        WHERE l.usuario IN :usuarios
+    """)
+    List<Lancamento> findByUsuarioIn(@Param("usuarios") List<Usuario> usuarios);
+
+    @Query("""
+        SELECT l FROM Lancamento l 
+        JOIN FETCH l.categoria 
+        JOIN FETCH l.usuario
+    """)
+    List<Lancamento> findAllWithCategoriaAndUsuario();
 
     List<Lancamento> findByUsuarioId(Integer id);
 
-    @Query("SELECT l FROM Lancamento l JOIN FETCH l.categoria WHERE l.usuario IN :usuarios")
-    List<Lancamento> findByUsuarioIn(List<Usuario> usuarios);
-
     Optional<Lancamento> findByDescricaoAndValorAndDataAndUsuario(String descricao, BigDecimal valor, LocalDate data, Usuario usuario);
-
-    @Query("SELECT l FROM Lancamento l JOIN FETCH l.categoria JOIN FETCH l.usuario")
-    List<Lancamento> findAllWithCategoriaAndUsuario();
 
 
 }
